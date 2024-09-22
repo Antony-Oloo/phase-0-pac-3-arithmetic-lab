783ea792949576d94a2d69139d0c073e88749834
antony <antoloo254@gmail.com>
2024-09-22 23:14:56 +0100

    Completed number manipulation and parsing functions

---
+++ index.js
@@ -0,0 +1,33 @@
+//Code to show basic math operations
+//adding two numbers 
+function add(a,b){
+return a+b;
+}
+//substracting two numbers
+function subtract(a,b){
+    return a-b;
+}
+//diving two numbers
+function divide(a,b){
+    return a/b;
+}
+//multiplying two numbers
+function multiply(a,b){
+    return a*b;
+}
+//Increments a number by 1
+function increment(n){
+    return n+1;
+}
+// Decrements a number by 1
+function decrement(n){
+    return n-1;
+}
+// Parses a string as an integer in base 10
+function makeInt(string) {
+    return parseInt(string, 10);
+  }
+  // Parses a string as a floating-point number
+  function preserveDecimal(string) {
+    return parseFloat(string);
+  }
